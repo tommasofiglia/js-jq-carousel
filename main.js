e4b8098b65b4324ff79c9313828a317313c81f8e
Tommaso Figlia <tommasofiglia@gmail.com>
2020-11-27 16:01:43 +0100

    Commit iniziale con prime righe di codice

---
+++ main.js
@@ -0,0 +1,28 @@
+//Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+//Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
+//Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
+//Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+
+/*********************** SVOLGIMENTO **********************/
+
+//Uso la forma contratta di Document ready per far sì che il codice js venga letto dal browser dopo il caricamento del DOM.
+
+$(function() {
+
+var arrowLeft = $(".prev");
+var arrowRight = $(".next");
+
+var imgSlider = $()
+
+
+arrowLeft.click(function() {
+console.log("test");
+});
+
+arrowRight.click(function() {
+console.log("test");
+});
+
+
+
+});
